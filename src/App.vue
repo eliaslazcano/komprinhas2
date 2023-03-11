21659eb061debd9a30db7bfc828e623b67e4d284
Elias Lazcano Castro Neto <elias304162@gmail.com>
2023-03-11 00:21:46 -0400

    Modo dark

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,18 @@
 </template>
 
 <script>
-import { defineComponent } from 'vue'
-
+import { defineComponent, watch } from 'vue'
+import { useQuasar } from 'quasar'
 export default defineComponent({
-  name: 'App'
+  name: 'App',
+  setup() {
+    const $q = useQuasar()
+    const darkStorage = localStorage.getItem('dark')
+    if (darkStorage !== null) $q.dark.set(JSON.parse(darkStorage))
+    else $q.dark.set(true)
+    watch(() => $q.dark.isActive, val => {
+      localStorage.setItem('dark', JSON.stringify(val))
+    })
+  }
 })
 </script>
--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,41 +1,27 @@
 <template>
-  <q-layout view="lHh Lpr lFf">
-    <q-header elevated>
+  <q-layout view="hHh Lpr lFf">
+    <q-header
+      :bordered="$q.dark.isActive"
+      :elevated="!$q.dark.isActive"
+      :class="{'bg-dark':$q.dark.isActive}"
+    >
       <q-toolbar>
+        <q-btn flat dense round icon="menu" aria-label="Menu" @click="toggleLeftDrawer" />
+        <q-toolbar-title>Komprinhas</q-toolbar-title>
         <q-btn
           flat
-          dense
           round
-          icon="menu"
-          aria-label="Menu"
-          @click="toggleLeftDrawer"
+          :icon="$q.dark.isActive ? 'light_mode' : 'dark_mode'"
+          @click="$q.dark.toggle"
         />
-
-        <q-toolbar-title>
-          Quasar App
-        </q-toolbar-title>
-
-        <div>Quasar v{{ $q.version }}</div>
       </q-toolbar>
     </q-header>
 
     <q-drawer
-      v-model="leftDrawerOpen"
-      show-if-above
-      bordered
-    >
+      v-model="leftDrawerOpen" show-if-above bordered>
       <q-list>
-        <q-item-label
-          header
-        >
-          Essential Links
-        </q-item-label>
-
-        <EssentialLink
-          v-for="link in essentialLinks"
-          :key="link.title"
-          v-bind="link"
-        />
+        <q-item-label header>Ferramentas</q-item-label>
+        <EssentialLink v-for="link in essentialLinks" :key="link.title" v-bind="link" />
       </q-list>
     </q-drawer>
 
@@ -48,69 +34,57 @@
 <script>
 import { defineComponent, ref } from 'vue'
 import EssentialLink from 'components/EssentialLink.vue'
-
-const linksList = [
-  {
-    title: 'Docs',
-    caption: 'quasar.dev',
-    icon: 'school',
-    link: 'https://quasar.dev'
-  },
-  {
-    title: 'Github',
-    caption: 'github.com/quasarframework',
-    icon: 'code',
-    link: 'https://github.com/quasarframework'
-  },
-  {
-    title: 'Discord Chat Channel',
-    caption: 'chat.quasar.dev',
-    icon: 'chat',
-    link: 'https://chat.quasar.dev'
-  },
-  {
-    title: 'Forum',
-    caption: 'forum.quasar.dev',
-    icon: 'record_voice_over',
-    link: 'https://forum.quasar.dev'
-  },
-  {
-    title: 'Twitter',
-    caption: '@quasarframework',
-    icon: 'rss_feed',
-    link: 'https://twitter.quasar.dev'
-  },
-  {
-    title: 'Facebook',
-    caption: '@QuasarFramework',
-    icon: 'public',
-    link: 'https://facebook.quasar.dev'
-  },
-  {
-    title: 'Quasar Awesome',
-    caption: 'Community Quasar projects',
-    icon: 'favorite',
-    link: 'https://awesome.quasar.dev'
-  }
-]
-
 export default defineComponent({
   name: 'MainLayout',
-
-  components: {
-    EssentialLink
-  },
-
+  components: { EssentialLink },
   setup () {
-    const leftDrawerOpen = ref(false)
-
-    return {
-      essentialLinks: linksList,
-      leftDrawerOpen,
-      toggleLeftDrawer () {
-        leftDrawerOpen.value = !leftDrawerOpen.value
+    const essentialLinks = [
+      {
+        title: 'Docs',
+        caption: 'quasar.dev',
+        icon: 'school',
+        link: 'https://quasar.dev'
+      },
+      {
+        title: 'Github',
+        caption: 'github.com/quasarframework',
+        icon: 'code',
+        link: 'https://github.com/quasarframework'
+      },
+      {
+        title: 'Discord Chat Channel',
+        caption: 'chat.quasar.dev',
+        icon: 'chat',
+        link: 'https://chat.quasar.dev'
+      },
+      {
+        title: 'Forum',
+        caption: 'forum.quasar.dev',
+        icon: 'record_voice_over',
+        link: 'https://forum.quasar.dev'
+      },
+      {
+        title: 'Twitter',
+        caption: '@quasarframework',
+        icon: 'rss_feed',
+        link: 'https://twitter.quasar.dev'
+      },
+      {
+        title: 'Facebook',
+        caption: '@QuasarFramework',
+        icon: 'public',
+        link: 'https://facebook.quasar.dev'
+      },
+      {
+        title: 'Quasar Awesome',
+        caption: 'Community Quasar projects',
+        icon: 'favorite',
+        link: 'https://awesome.quasar.dev'
       }
-    }
+    ]
+    const leftDrawerOpen = ref(false)
+    const toggleLeftDrawer = () => leftDrawerOpen.value = !leftDrawerOpen.value
+    return { essentialLinks, leftDrawerOpen, toggleLeftDrawer }
   }
 })
 </script>
