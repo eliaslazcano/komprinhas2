08d320ef7c9ff90ca70710a17b90421c7cf3f2af
Elias Lazcano Castro Neto <elias304162@gmail.com>
2023-03-11 01:03:30 -0400

    Menu de navegação

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -40,47 +40,23 @@ export default defineComponent({
   setup () {
     const essentialLinks = [
       {
-        title: 'Docs',
-        caption: 'quasar.dev',
-        icon: 'school',
-        link: 'https://quasar.dev'
+        title: 'Carrinho de compras',
+        caption: '',
+        icon: 'shopping_cart',
+        to: '/carrinhos'
       },
       {
-        title: 'Github',
-        caption: 'github.com/quasarframework',
-        icon: 'code',
-        link: 'https://github.com/quasarframework'
+        title: 'Lista de compras',
+        caption: '',
+        icon: 'format_list_bulleted',
+        to: '/listas'
       },
       {
-        title: 'Discord Chat Channel',
-        caption: 'chat.quasar.dev',
-        icon: 'chat',
-        link: 'https://chat.quasar.dev'
+        title: 'Produtos',
+        caption: '',
+        icon: 'liquor',
+        to: '/produtos'
       },
-      {
-        title: 'Forum',
-        caption: 'forum.quasar.dev',
-        icon: 'record_voice_over',
-        link: 'https://forum.quasar.dev'
-      },
-      {
-        title: 'Twitter',
-        caption: '@quasarframework',
-        icon: 'rss_feed',
-        link: 'https://twitter.quasar.dev'
-      },
-      {
-        title: 'Facebook',
-        caption: '@QuasarFramework',
-        icon: 'public',
-        link: 'https://facebook.quasar.dev'
-      },
-      {
-        title: 'Quasar Awesome',
-        caption: 'Community Quasar projects',
-        icon: 'favorite',
-        link: 'https://awesome.quasar.dev'
-      }
     ]
     const leftDrawerOpen = ref(false)
     const toggleLeftDrawer = () => leftDrawerOpen.value = !leftDrawerOpen.value
